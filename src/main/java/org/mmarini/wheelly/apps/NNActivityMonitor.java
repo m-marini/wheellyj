27120ec8ba1f7bffc5adf587a9fe7b6f4e77fab2
m-marini <marco.marini@mmarini.org>
2024-03-19 17:57:30 +0100

    Added neural activity map application #390

---
+++ src/main/java/org/mmarini/wheelly/apps/NNActivityMonitor.java
@@ -0,0 +1,355 @@
+/*
+ * Copyright (c) 2024 Marco Marini, marco.marini@mmarini.org
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *    END OF TERMS AND CONDITIONS
+ *
+ */
+
+package org.mmarini.wheelly.apps;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import hu.akarnokd.rxjava3.swing.SwingObservable;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+import org.mmarini.rl.agents.Agent;
+import org.mmarini.rl.agents.BinArrayFile;
+import org.mmarini.rl.agents.KeyFileMap;
+import org.mmarini.rl.agents.TDAgentSingleNN;
+import org.mmarini.rl.nets.TDNetwork;
+import org.mmarini.swing.GridLayoutHelper;
+import org.mmarini.swing.SwingUtils;
+import org.mmarini.wheelly.envs.RobotEnvironment;
+import org.mmarini.wheelly.swing.Messages;
+import org.mmarini.wheelly.swing.NNActivityPanel;
+import org.mmarini.wheelly.swing.Utils;
+import org.mmarini.yaml.Locator;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import static java.lang.String.format;
+import static org.mmarini.wheelly.apps.Wheelly.WHEELLY_SCHEMA_YML;
+import static org.mmarini.wheelly.swing.Utils.createFrame;
+import static org.mmarini.yaml.Utils.fromFile;
+
+/**
+ * Application to monitor neural network activity
+ */
+public class NNActivityMonitor {
+    private static final Logger logger = LoggerFactory.getLogger(NNActivityMonitor.class);
+    public static final int SIM_PERIOD = 100;
+    public static final int PLAY_DIVIDER = 3;
+
+    static {
+        Nd4j.zeros(1);
+    }
+
+    /**
+     * Returns the argument parser
+     */
+    private static ArgumentParser createParser() {
+        ArgumentParser parser = ArgumentParsers.newFor(ToCsv.class.getName()).build()
+                .defaultHelp(true)
+                .version(Messages.getString("Wheelly.title"))
+                .description("Run a session of batch training.");
+        parser.addArgument("-v", "--version")
+                .action(Arguments.version())
+                .help("show current version");
+        parser.addArgument("-c", "--config")
+                .setDefault("wheelly.yml")
+                .help("specify yaml configuration file");
+        parser.addArgument("kpis")
+                .type(String.class)
+                .help("the kpis path");
+        return parser;
+    }
+
+    /**
+     * Runs the app
+     *
+     * @param args the command line argument s
+     */
+    public static void main(String[] args) {
+        ArgumentParser parser = createParser();
+        try {
+            Namespace arguments = parser.parseArgs(args);
+            new NNActivityMonitor(arguments).run();
+        } catch (ArgumentParserException ex) {
+            parser.handleError(ex);
+            System.exit(1);
+        } catch (Throwable ex) {
+            logger.atError().setCause(ex).log("Error running application");
+            System.exit(8);
+        }
+    }
+
+    private final Namespace args;
+    private final NNActivityPanel activityPanel;
+    private final JSlider timeLine;
+    private final JButton rewind;
+    private final JToggleButton play;
+    private final JToggleButton fastPlay;
+    private final JButton stop;
+
+    private TDNetwork network;
+    private Map<String, BinArrayFile> signalFiles;
+    private boolean playing;
+    private int timeDivider;
+    private BinArrayFile refFile;
+
+    /**
+     * Loads the network
+     *
+     * @throws IOException in case of error
+     */
+    private void loadNetwork() throws IOException {
+        JsonNode config = fromFile(args.getString("config"));
+        RobotEnvironment environment = AppYaml.envFromJson(config, Locator.root(), WHEELLY_SCHEMA_YML);
+        Locator agentLocator = Locator.locate(Locator.locate("agent").getNode(config).asText());
+        if (agentLocator.getNode(config).isMissingNode()) {
+            throw new IllegalArgumentException(format("Missing node %s", agentLocator));
+        }
+        Agent agent = Agent.fromConfig(config, agentLocator, environment);
+        if (agent instanceof TDAgentSingleNN tdagent) {
+            this.network = tdagent.network();
+        } else {
+            throw new IllegalArgumentException(
+                    format("Wrong agent type %s", agent.getClass().getName()));
+        }
+    }
+
+    /**
+     * Creates the application
+     *
+     * @param args the parsed command line arguments
+     */
+    protected NNActivityMonitor(Namespace args) {
+        this.args = args;
+        this.activityPanel = new NNActivityPanel();
+        this.timeLine = new JSlider(JSlider.HORIZONTAL);
+        this.rewind = SwingUtils.createButton("NNActivityMonitor.rewind");
+        this.play = SwingUtils.createToolBarToggleButton("NNActivityMonitor.play");
+        this.fastPlay = SwingUtils.createToolBarToggleButton("NNActivityMonitor.fastPlay");
+        this.stop = SwingUtils.createButton("NNActivityMonitor.stop");
+
+        timeLine.setPaintTicks(true);
+        timeLine.setPaintLabels(true);
+        timeLine.setPaintTrack(true);
+        timeLine.setValue(0);
+
+        rewind.addActionListener(ev -> rewind());
+        stop.addActionListener(ev -> stop());
+        play.addActionListener(ev -> play());
+        fastPlay.addActionListener(ev -> fastPlay());
+        SwingObservable.change(timeLine)
+                .filter(ignored -> !timeLine.getValueIsAdjusting())
+                .doOnNext(ev -> moveTimeline())
+                .subscribe();
+    }
+
+    /**
+     * Returns the command bar
+     */
+    private Container createCommandBar() {
+        return new GridLayoutHelper<>(new JPanel()).modify("insets,5 center")
+                .modify("at,0,0").add(rewind)
+                .modify("at,1,0").add(stop)
+                .modify("at,2,0").add(play)
+                .modify("at,3,0").add(fastPlay)
+                .modify("at,4,0 hfill hw,1").add(timeLine)
+                .getContainer();
+    }
+
+    /**
+     * Returns the content panel
+     */
+    private Component createContentPanel() {
+        return new GridLayoutHelper<>(new JPanel()).modify("insets,5 center").modify("at,0,0 weight,1,1").add(activityPanel).modify("at,0,1 hw,1 fill").add(createCommandBar()).getContainer();
+    }
+
+    /**
+     * Runs player at fast speed
+     */
+    private void fastPlay() {
+        if (fastPlay.isSelected()) {
+            playing = true;
+            timeDivider = 1;
+            play.setSelected(false);
+            timeLine.setEnabled(false);
+        } else {
+            stop();
+        }
+    }
+
+    /**
+     * Loads the signals
+     */
+    private void loadSignals() {
+        String source = args.getString("kpis");
+        String[] sourceKeys = network.sourceLayers().stream()
+                .map(name -> "s0." + name)
+                .toArray(String[]::new);
+        File sourcePath = new File(source);
+        Map<String, BinArrayFile> signalFiles =
+                KeyFileMap.create(sourcePath, sourceKeys);
+        List<String> missingFiles = Arrays.stream(sourceKeys)
+                .filter(Predicate.not(signalFiles::containsKey))
+                .toList();
+        if (!missingFiles.isEmpty()) {
+            throw new RuntimeException(format("Missing kpis files %s",
+                    String.join(", ", missingFiles)));
+        }
+        this.signalFiles = KeyFileMap.children(signalFiles, "s0");
+    }
+
+    /**
+     * Move the timeline
+     */
+    private void moveTimeline() {
+        try {
+            int pos = timeLine.getValue();
+            if (refFile.position() != pos) {
+                stop();
+                KeyFileMap.seek(this.signalFiles, pos);
+                Map<String, INDArray> activity = processNextSignal();
+                if (activity != null) {
+                    activityPanel.setActivity(activity);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Runs player at normal speed
+     */
+    private void play() {
+        if (play.isSelected()) {
+            playing = true;
+            timeDivider = PLAY_DIVIDER;
+            fastPlay.setSelected(false);
+            timeLine.setEnabled(false);
+        } else {
+            stop();
+        }
+    }
+
+    /**
+     * Returns the next signal from player
+     */
+    private Map<String, INDArray> processNextSignal() throws IOException {
+        Map<String, INDArray> signals = KeyFileMap.read(signalFiles, 1);
+        if (signals == null) {
+            stop();
+            return null;
+        } else {
+            long pos = refFile.position();
+            timeLine.setValue((int) pos);
+            return network.forward(signals).values();
+        }
+    }
+
+    /**
+     * Rewind the player
+     */
+    private void rewind() {
+        try {
+            KeyFileMap.seek(signalFiles, 0);
+            Map<String, INDArray> activity = processNextSignal();
+            if (activity != null) {
+                activityPanel.setActivity(activity);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        timeLine.setValue(0);
+        playing = false;
+        play.setSelected(false);
+        fastPlay.setSelected(false);
+    }
+
+    /**
+     * Runs the application
+     */
+    private void run() throws IOException {
+        loadNetwork();
+
+        // Prepare the input data
+        loadSignals();
+
+        this.refFile = this.signalFiles.values().stream().findAny().orElseThrow();
+        long size = refFile.size();
+        timeLine.setMaximum((int) size);
+        timeLine.setMajorTickSpacing((int) (size / 5));
+        timeLine.setMinorTickSpacing((int) (size / 20));
+
+        activityPanel.configure(network);
+
+        JFrame frame = createFrame(Messages.getString("NNActivityMonitor.title"), createContentPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Utils.center(frame);
+        frame.setVisible(true);
+
+        rewind();
+
+        Flowable.interval(SIM_PERIOD, TimeUnit.MILLISECONDS).
+                observeOn(Schedulers.computation())
+                .filter(ignoerd -> playing)
+                .filter(t -> t % timeDivider == 0)
+                .doOnNext(ignored -> {
+                    Map<String, INDArray> activity = processNextSignal();
+                    if (activity != null) {
+                        activityPanel.setActivity(activity);
+                    }
+                })
+                .subscribe();
+    }
+
+    /**
+     * Stop the player
+     */
+    private void stop() {
+        playing = false;
+        play.setSelected(false);
+        fastPlay.setSelected(false);
+        timeLine.setEnabled(true);
+    }
+}
---
+++ src/main/java/org/mmarini/wheelly/swing/ActivityMap.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2024 Marco Marini, marco.marini@mmarini.org
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *    END OF TERMS AND CONDITIONS
+ *
+ */
+
+package org.mmarini.wheelly.swing;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.lang.Math.*;
+import static org.mmarini.wheelly.apis.Utils.linear;
+
+/**
+ * Display the activity of a layer
+ */
+public class ActivityMap extends JComponent {
+
+    public static final int PREFERRED_SIZE = 100;
+    public static final int MIN_DOT_SIZE = 3;
+    public static final float MIN_RANGE = 1e-6F;
+    public static final float DECAY_1 = 100e-3F;
+
+    /**
+     * Returns the color for a give temperature
+     *
+     * @param temp the temperature
+     */
+    private static Color colorTemperature(float temp) {
+        float hue = temp * 5F / 6;
+        float bright = (1 - temp) * 0.6F + 0.4F;
+        return Color.getHSBColor(hue, 1, bright);
+        //return Color.getHSBColor(0, 0, 1-temp);
+    }
+
+    private int activityWidth;
+    private int activityHeight;
+    private int dotSize;
+    private Color[] activity;
+    private INDArray avg;
+
+    /**
+     * Creates the activity map
+     */
+    public ActivityMap() {
+        this.dotSize = 8;
+        this.activity = new Color[0];
+        setBackground(Color.BLACK);
+    }
+
+    @Override
+    protected void paintComponent(Graphics gr) {
+        Insets insets = getInsets();
+        if (insets == null) {
+            insets = new Insets(0, 0, 0, 0);
+        }
+        int width = getWidth() - insets.left - insets.right;
+        int height = getHeight() - insets.top - insets.bottom;
+        Graphics g = gr.create(insets.left, insets.top, width, height);
+        g.setColor(getBackground());
+        g.clipRect(0, 0, width, height);
+        g.fillRect(0, 0, width, height);
+        Color[] activity = this.activity;
+        for (int i = 0; i < activity.length; i++) {
+            int xi = i % activityWidth;
+            int yi = i / activityWidth;
+            int x = xi * width / activityWidth;
+            int w = (xi + 1) * width / activityWidth - x;
+            int y = yi * height / activityHeight;
+            int h = (yi + 1) * height / activityHeight - y;
+            g.setColor(activity[i]);
+            g.fillRect(x, y, w, h);
+        }
+    }
+
+    /**
+     * Sets the activity values
+     *
+     * @param activity the activity
+     */
+    void setActivity(INDArray activity) {
+        if (avg == null) {
+            avg = activity.dup();
+        } else {
+            // avg = avg * decay + act * (1 - decay)
+            // avg = avg * decay + act - act * decay
+            // avg = (avg - act) * decay + act
+
+            // avg = avg * decay + avg * (1-decay) - avg * (1-decay) + act * (1 - decay)
+            // avg = avg * decay + avg * (1-decay) + (act-avg) * (1-decay)
+            // avg =  avg + (act-avg) * (1-decay)
+            // avg =  avg + (act-avg) * (1-decay)
+            try (INDArray delta = activity.sub(avg).muli(DECAY_1)) {
+                avg.addi(delta);
+            }
+        }
+        int size = activityHeight * activityWidth;
+        Color[] activityColors = new Color[(int) min(size, avg.size(1))];
+        float min = avg.minNumber().floatValue();
+        float max = avg.maxNumber().floatValue();
+        float range = max - min;
+        if (range < MIN_RANGE) {
+            float h = max(abs(max) + MIN_RANGE, abs(min) + MIN_RANGE);
+            min = -h;
+            max = h;
+        }
+        for (int i = 0; i < avg.size(1) && i < size; i++) {
+            float temp = linear(avg.getFloat(0, i), min, max, 0, 1);
+            activityColors[i] = colorTemperature(1 - temp);
+        }
+        this.activity = activityColors;
+        repaint();
+    }
+
+    /**
+     * Set the layer size
+     *
+     * @param width  the width
+     * @param height the height
+     */
+    void setLayerSize(int width, int height) {
+        Insets insets = getInsets();
+        if (insets == null) {
+            insets = new Insets(0, 0, 0, 0);
+        }
+        this.activityWidth = width;
+        this.activityHeight = height;
+        dotSize = max(PREFERRED_SIZE / max(activityWidth, activityHeight), MIN_DOT_SIZE);
+        setPreferredSize(new Dimension(
+                activityWidth * dotSize + insets.left + insets.right,
+                activityHeight * dotSize + insets.top + insets.bottom));
+        repaint();
+    }
+}
