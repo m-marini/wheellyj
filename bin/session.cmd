a579b3ec1ee9412ff5d3bf14431a8c7a006a6e74
Marco Marini <marco.marini@mmarini.org>
2025-04-29 15:23:14 +0200

    Missing reward function

--- bin/session.cmd
+++ bin/session.cmd
@@ -8,7 +8,7 @@ rem  Run session
 call bin\wheelly.cmd -k %KPIS% -t 21600 -a -w -s -i %INFERENCE%
 
 rem Compute report data
-call bin\report.cmd -p %DATA% %MODEL% %CSVS%
+call bin\report.cmd -p %KPIS% %MODEL% %CSVS%
 
 rem Generate report document
 cd octave
